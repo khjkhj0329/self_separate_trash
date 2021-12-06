a9f38760a363e73f9117e4e471e34fa513c5d188
효정이 <w2029@e-mirim.hs.kr>
2021-12-07 02:20:15 +0900

    쓰레기, 재활용 버리는 법 디자인 완성(java클래스, activity)

---
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/DressActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.w2029.self_separate_trash;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DressActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dress);
+    }
+}
---
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/Planstic2Activity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.w2029.self_separate_trash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Planstic2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planstic2);
+    }
+}

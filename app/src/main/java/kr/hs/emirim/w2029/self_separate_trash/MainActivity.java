6d367ec055d26743f825d6614c39d064adb48bf0
효정이 <w2029@e-mirim.hs.kr>
2021-12-06 11:16:44 +0900

    자동으로 화면 넘어가기 구성 완료

--- app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.hs.emirim.w2029.self_separate_trash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent start = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(start);
+                finish();
+            }
+        }, 2000);
     }
 }

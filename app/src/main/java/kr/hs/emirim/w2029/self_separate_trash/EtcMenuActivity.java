5d58b435a0447a5752272578d185864851a68d12
효정이 <w2029@e-mirim.hs.kr>
2021-12-07 02:20:58 +0900

    쓰레기 id 부여 후 화면 전환 하기

--- app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/EtcMenuActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/EtcMenuActivity.java
@@ -6,10 +6,11 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EtcMenuActivity extends AppCompatActivity {
-    ImageButton backBtn, homeBtn, menuBtn;
+    ImageButton backBtn, homeBtn, menuBtn, foodBtn, plansticBtn, dressBtn, styrofoamBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,43 @@ public class EtcMenuActivity extends AppCompatActivity {
         homeBtn = findViewById(R.id.home_btn);
         menuBtn = findViewById(R.id.menu_btn);
 
+        foodBtn = findViewById(R.id.planstic_btn);
+        plansticBtn = findViewById(R.id.dress_btn);
+        dressBtn = findViewById(R.id.styrofoam_btn);
+        styrofoamBtn = findViewById(R.id.food_btn);
+
+        foodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getApplicationContext(), FoodActivity.class);
+                startActivity(intent3);
+                finish();
+            }
+        });
+        plansticBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getApplicationContext(), PlansticActivity.class);
+                startActivity(intent3);
+                finish();
+            }
+        });
+        dressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getApplicationContext(), DressActivity.class);
+                startActivity(intent3);
+                finish();
+            }
+        });
+        styrofoamBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getApplicationContext(), StyrofoamActivity.class);
+                startActivity(intent3);
+                finish();
+            }
+        });
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/RecyclingActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/RecyclingActivity.java
@@ -5,22 +5,70 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecyclingActivity extends AppCompatActivity {
-    ImageButton bakBtn, homeBtn;
+    ImageButton menuBtn, bakBtn, homeBtn, canBtn,glassBtn, paperBtn, planstic_Btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycling_menu);
 
+
         bakBtn = findViewById(R.id.back_btn);
         homeBtn = findViewById(R.id.home_btn);
+        menuBtn = findViewById(R.id.menu_btn);
 
+        canBtn = findViewById(R.id.can_btn);
+        glassBtn = findViewById(R.id.glass_btn);
+        paperBtn = findViewById(R.id.paper_btn);
+        planstic_Btn = findViewById(R.id.planstic_btn);
+
+        canBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CanActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        glassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GlassActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        paperBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PaperActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        planstic_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Planstic2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EtcMenuActivity.class);
                 startActivity(intent);
                 finish();
             }

63e6e63e4ec538b1eb2f53009b61bfd94202451e
효정이 <w2029@e-mirim.hs.kr>
2021-12-07 02:21:38 +0900

    검색 버튼 id 부여 후 화면 전환

--- app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/StartActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/StartActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
     ImageButton recyclingBtn, menuBtn;
+    TextView searchBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,15 @@ public class StartActivity extends AppCompatActivity {
 
         recyclingBtn = findViewById(R.id.recycling_btn);
         menuBtn = findViewById(R.id.menu_btn);
+        searchBtn = findViewById(R.id.search_btn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

f7ba3363aaeb59c8c755c69e194c6eec4ded2c1e
효정이 <w2029@e-mirim.hs.kr>
2021-12-06 11:16:26 +0900

    디자인 완성

--- app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/StartActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/StartActivity.java
@@ -1,13 +1,29 @@
 package kr.hs.emirim.w2029.self_separate_trash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
+    ImageButton recyclingBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start);
+
+        recyclingBtn = findViewById(R.id.recycling_btn);
+        recyclingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RecyclingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }

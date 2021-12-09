2ec19105b824daa2548030ae1688e052a80d79ae
효정이 <w2029@e-mirim.hs.kr>
2021-12-10 00:58:01 +0900

    로그아웃 구현 완성

--- app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/StartActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/StartActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.w2029.self_separate_trash;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +8,12 @@ import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
     ImageButton recyclingBtn, menuBtn, trashBtn, mypageBtn;
-    TextView searchBtn;
+    TextView logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +24,36 @@ public class StartActivity extends AppCompatActivity {
         menuBtn = findViewById(R.id.menu_btn);
 //        searchBtn = findViewById(R.id.search_btn);
         trashBtn = findViewById(R.id.trash_btn);
-        mypageBtn = findViewById(R.id.mypage_btn);
+        logoutBtn = findViewById(R.id.log_out);
 
-        mypageBtn.setOnClickListener(new View.OnClickListener() {
+        // 로그아웃
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), InsertActivity.class);
-                startActivity(intent);
-                finish();
+                AlertDialog.Builder builder = new AlertDialog.Builder(StartActivity.this);
+                builder.setMessage("로그아웃 하시겠습니까?");
+                builder.setTitle("로그아웃 알림창")
+                        .setCancelable(false)
+                        .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("로그아웃");
+                alert.show();
             }
         });
+
 //        searchBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {

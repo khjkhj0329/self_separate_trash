22bd040b40f3e0c51fec93823740ab5b5192ee96
효정이 <w2029@e-mirim.hs.kr>
2021-12-10 00:37:32 +0900

    마이페이지 시작

---
+++ app/src/main/java/kr/hs/emirim/w2029/self_separate_trash/MypageActivity.java
@@ -0,0 +1,27 @@
+package kr.hs.emirim.w2029.self_separate_trash;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+
+
+
+    }
+}
